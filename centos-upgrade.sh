598737d471c2db6fec0cda046c902937e0cf19f4
‘josh <kingorijosphat@yahoo.com>
2022-10-06 13:56:38 -0400

    My first automation script

---
+++ centos-upgrade.sh
@@ -0,0 +1,19 @@
+#!/usr/bin
+
+sudo yum upgrade -y 
+
+sudo yum install net-tools -y 
+
+sudo yum install wget -y 
+
+sudo yum install git -y 
+
+sudo useradd josh -y
+
+sudo nproc 
+
+sudo lsblk 
+
+sudo top 
+
+sudo pwd 
